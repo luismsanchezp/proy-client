1305b266f1f9c7b6ba15cb5e5781cbfa953f1fda
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-10-20 10:11:42 -0500

    Implement React.Fragment navigation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
+import React from 'react';
 import './App.scss';
-import { Card, DatePicker, Alert } from 'antd';
+import Admin from './pages/admin';
+import Home from './pages/home';
+import Contact from './pages/contact';
 
 function App() {
-  const date_test = (date, dateString) => {
-    console.log(date, dateString)
-  }
   return (
-    <div className="App">
-      <h1>Componentes Ant Design</h1>
-      <label>Ingenieria de Software II</label>
-      <div>
-        <Alert message='Success text' type='success' />
-        <Alert message='Info text' type='info' />
-        <Alert message='Warning text' type='warning' />
-        <Alert message='Error text' type='error' />
-      </div>
-      <label>Seleccione una fecha</label>
-      <DatePicker on onChange={date_test}></DatePicker>
-      <div>
-        <Card
-          size='small'
-          title='Tarjeta de presentacion'
-          extra={<a href="#">Ver mas</a>}
-          style={{ width:300 }}>
-            <p>Luis Miguel Sanchez Pinilla</p>
-            <p>Estudiante Ingenieria de Sistemas</p>
-        </Card>
-      </div>
-    </div>
+    <React.Fragment>
+      <Admin></Admin>
+      <Home></Home>
+      <Contact></Contact>
+    </React.Fragment>
   );
 }
 
