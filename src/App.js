f3ada215df59fc464a2e7e2d3281e031825e28fa
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-10-20 09:16:32 -0500

    Add ant-design framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.scss';
+import { Card, DatePicker, Alert } from 'antd';
 
 function App() {
+  const date_test = (date, dateString) => {
+    console.log(date, dateString)
+  }
   return (
     <div className="App">
-      <h1>Prueba proyecto</h1>
+      <h1>Componentes Ant Design</h1>
+      <label>Ingenieria de Software II</label>
+      <div>
+        <Alert message='Success text' type='success' />
+        <Alert message='Info text' type='info' />
+        <Alert message='Warning text' type='warning' />
+        <Alert message='Error text' type='error' />
+      </div>
+      <label>Seleccione una fecha</label>
+      <DatePicker on onChange={date_test}></DatePicker>
+      <div>
+        <Card
+          size='small'
+          title='Tarjeta de presentacion'
+          extra={<a href="#">Ver mas</a>}
+          style={{ width:300 }}>
+            <p>Luis Miguel Sanchez Pinilla</p>
+            <p>Estudiante Ingenieria de Sistemas</p>
+        </Card>
+      </div>
     </div>
   );
 }
